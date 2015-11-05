77cb9b8aecda0e8e7e2e5f5e3263281945394664
msdx <645079761@qq.com>
2015-11-05 14:16:10 +0800

    AndroidManifest添加版本号

--- app/src/main/java/com/githang/gradledoc/others/AboutActivity.java
+++ app/src/main/java/com/githang/gradledoc/others/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.githang.gradledoc.others;
 
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.githang.gradledoc.BuildConfig;
 import com.githang.gradledoc.R;
 import com.githang.gradledoc.common.BaseBackActivity;
 import com.umeng.analytics.MobclickAgent;
@@ -21,7 +23,15 @@ public class AboutActivity extends BaseBackActivity {
     }
 
     private void setVersion() {
-        mVersion.setText("版本: v" + BuildConfig.VERSION_NAME);
+        ApplicationInfo info = this.getApplicationInfo();
+        mVersion.setText("版本: ");
+        try {
+            PackageInfo packageInfo= getPackageManager().getPackageInfo(info.packageName, 0);
+            mVersion.append(" V");
+            mVersion.append(packageInfo.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
 
